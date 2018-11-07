533ff2d7b66184ae67ca82574b81ae831cc70af3
daigouwei <daigouwei@gmail.com>
2018-11-07 18:01:15 +0800

    add BaseService

---
+++ log-app-common/src/main/java/com/gg/applog/common/service/BaseService.java
@@ -0,0 +1,12 @@
+package com.gg.applog.common.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author daigouwei
+ * @date 2018/11/7
+ */
+public class BaseService {
+    protected Logger LOG = LoggerFactory.getLogger("service");
+}
