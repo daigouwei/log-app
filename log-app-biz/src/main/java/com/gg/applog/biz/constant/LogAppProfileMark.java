0d2fb2b9d3c94e4dab20143096f02b3464521210
daigouwei <daigouwei@gmail.com>
2018-11-09 10:07:46 +0800

    modify LogAppProfileMark serializable

--- log-app-biz/src/main/java/com/gg/applog/biz/constant/LogAppProfileMark.java
+++ log-app-biz/src/main/java/com/gg/applog/biz/constant/LogAppProfileMark.java
@@ -1,10 +1,13 @@
 package com.gg.applog.biz.constant;
 
+import scala.Serializable;
+
 /**
  * @author daigouwei
  * @date 2018/11/7
  */
-public class LogAppProfileMark {
+public class LogAppProfileMark implements Serializable {
+    private static final long serialVersionUID = 1L;
     public static final String PROFILE_PROD_ID = "!!";
     public static final String PROFILE_CAT_CODE = "##";
     public static final String PROFILE_KEYWORD = "@@";
