d36cdab1d675fe6409a7caded0cad346f3c3bc6d
daigouwei <daigouwei@gmail.com>
2018-11-22 09:35:39 +0800

    modify LogSparkDriverImpl2

--- log-app-biz/src/main/java/com/gg/applog/biz/service/logspark/LogSparkDriverImpl2.java
+++ log-app-biz/src/main/java/com/gg/applog/biz/service/logspark/LogSparkDriverImpl2.java
@@ -10,11 +10,18 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import scala.Serializable;
 import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author daigouwei
@@ -86,9 +93,19 @@ public class LogSparkDriverImpl2 extends BaseService implements Serializable {
         LOG.info("store into mysql...");
         //转换为RDD
         JavaRDD<LogAppDBField> logAppDBFieldJavaRDD = logAppDBFieldJavaPairRDD.map(Tuple2::_2);
-        Dataset<Row> logAppDBFieldDF = sparkSession.createDataFrame(logAppDBFieldJavaRDD, LogAppDBField.class);
+        //第一步：在RDD的基础上创建类型为Row的RDD,Row可以简单理解为Table的一行数据
+        JavaRDD<Row> logAppDBFieldRowJavaRDD = logAppDBFieldJavaRDD.map(logAppDBField -> RowFactory
+                .create(logAppDBField.getAppId(), logAppDBField.getUserId(), logAppDBField.getProfile()));
+        //第二步：动态构造DataFrame的元数据
+        List<StructField> stuctFields = new ArrayList<>();
+        stuctFields.add(DataTypes.createStructField("app_id", DataTypes.StringType, true));
+        stuctFields.add(DataTypes.createStructField("user_id", DataTypes.StringType, true));
+        stuctFields.add(DataTypes.createStructField("profile", DataTypes.StringType, true));
+        StructType structType = DataTypes.createStructType(stuctFields);
+        //第三步：基于已有的元数据以及RDD<Row>来构建DataFrame
+        Dataset<Row> logAppDBFieldDF = sparkSession.createDataFrame(logAppDBFieldRowJavaRDD, structType);
+        //第四步：将数据写入到表中
         logAppDBFieldDF.write().mode("append")
-            .option("createTableColumnTypes", "app_id VARCHAR(50), user_id VARCHAR(50), profile VARCHAR(4000)")
-            .jdbc(logAppSparkSqlImpl.getUrl(), logAppSparkSqlImpl.getTable(), logAppSparkSqlImpl.getConnProperties());
+                .jdbc(logAppSparkSqlImpl.getUrl(), logAppSparkSqlImpl.getTable(), logAppSparkSqlImpl.getConnProperties());
     }
 }
